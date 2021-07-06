1f92a8975729f22136a550dd6a46c6e01bfdc12b
Demitroi <checo1085@hotmail.com>
2021-07-05 22:19:54 -0700

    first commit

---
+++ 1_imprimir.py
@@ -0,0 +1,10 @@
+# La forma mas simple de hacer un print
+print("Hola Mundo")
+
+# De se le puede indicar el final con el parametro end
+print("A", end='.\n')
+print("B", end='.\n')
+print("C", end='.\n')
+
+# Se pueden separar los mensajes por lo que se indique en el parametro end
+print("Naranja", "Sandia", "Platano", sep=". ")
