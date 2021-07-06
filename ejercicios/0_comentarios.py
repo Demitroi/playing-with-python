5fd6e85a2e861a74ce4930376946e04b769bc464
Demitroi <checo1085@hotmail.com>
2021-07-05 23:47:05 -0700

    Diferentes formas de poner comentarios

---
+++ ejercicios/0_comentarios.py
@@ -0,0 +1,14 @@
+# Hay tres formas de hacer comentarios
+# La primera es esta con el símbolo #
+# Al principio de cada linea
+
+'''
+La segunda es de varias lineas, poniendo
+tres comillas simples al principio y
+al final del comentario
+'''
+
+"""
+La tercera y ultima es con tres comillas
+dobles al principio y al final
+"""
