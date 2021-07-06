d4f156567624c8552ac0197aa817cd043d1a7285
Demitroi <checo1085@hotmail.com>
2021-07-05 23:12:00 -0700

    Ejercicio de input

---
+++ ejercicios/2_input.py
@@ -0,0 +1,4 @@
+# Con la función input se puede leer fácilmente desde el stdin
+frase = input("Escriba una frase inspiradora: ")
+# Podemos almacenar la frase inspiradora en una variable y despues imprimirla
+print("Esta es su frase inspiradora:", frase)
